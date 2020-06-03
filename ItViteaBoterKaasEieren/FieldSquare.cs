cbef63a68c7d9075e63d05198605885d38b73730
Stella van der burg <luna4s108@gmail.com>
2020-06-03 12:08:10 +0200

    Started building base fundaments for game. Including grid and classes. Still quite loose and might remove or add classes as needed.

---
+++ ItViteaBoterKaasEieren/FieldSquare.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItViteaBoterKaasEieren
+{
+    /// <summary>
+    /// Simple class to hold fieldstates and symbols to be displayed together.
+    /// </summary>
+    public class FieldSquare : INotifyPropertyChanged
+    {
+        #region private var for public properties
+        private FieldStates _FieldState;
+        private string _Symbol;
+        #endregion
+
+
+        #region public properties
+        public FieldStates FieldState
+        {
+            get { return _FieldState; }
+            set
+            {
+                _FieldState = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Symbol
+        {
+            get { return _Symbol; }
+            set
+            {
+                _Symbol = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        public enum FieldStates
+        {
+            None,
+            Cross,
+            Circle
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
---
+++ ItViteaBoterKaasEieren/PlayFieldVM.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItViteaBoterKaasEieren 
+{
+    public class PlayFieldVM : INotifyPropertyChanged
+    {
+        #region private var for public properties
+        private FieldSquare[,] _PlayingField;
+        private int _TurnCounter;
+        private bool _IsPlayer1;
+        #endregion
+
+        public PlayFieldVM ()
+        {
+            _PlayingField = new FieldSquare[3,3];
+            StartGame();
+        }
+
+        #region public properties
+        public FieldSquare[,] PlayingField
+        {
+            get { return _PlayingField; }
+            set
+            {
+                _PlayingField = value;
+                OnPropertyChanged();
+            }
+        }
+        public int TurnCounter
+        {
+            get { return _TurnCounter; }
+            set
+            {
+                _TurnCounter = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsPlayer1
+        {
+            get { return _IsPlayer1; }
+            set
+            {
+                _IsPlayer1 = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region methods
+        public void StartGame()
+        {
+            ResetPlayField();
+            IsPlayer1 = true;
+            TurnCounter = 0;
+        }
+        public void NextTurn()
+        {
+            //Switch between player 1's turn and player 2's turn.
+            if (IsPlayer1)
+                IsPlayer1 = false;
+            else
+                IsPlayer1 = true;
+            TurnCounter++;
+        }
+        public void ResetPlayField()
+        {
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    //square = new FieldSquare();
+                    //square.FieldState = FieldSquare.FieldStates.None;
+                }
+            }
+        }
+
+        #region SquareField Specific Methods
+    
+        #endregion
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+
+}
